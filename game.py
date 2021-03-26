a15b740bc170e78d4f8cd0d7de80e4791ee5bfe3
testusercpen333 <rabidh0b0@hotmail.com>
2021-03-25 20:27:13 -0700

    added main game loop

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ class Game:
         self.lives = 10
         self.money = 100
         self.bg = pygame.image.load(os.path.join("game_assets", "td-tilesets1-2", "tower-defense-game-tilsets", "PNG", "game_background_2", "layers", "game_background_2.png"))
+        self.clicks = []
 
     def run(self):
         run = True
@@ -19,9 +20,19 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                pos = pygame.mouse.get_pos()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.clicks.append(pos)
+                    print(pos)
+
             self.draw()
         pygame.quit()
 
     def draw(self):
         self.win.blit(self.bg, (0,0))
+        for p in self.clicks:
+            pygame.draw.circle(self.win, (255,0,0), (p[0], [1]), 5, 0)
         pygame.display.update()
+
+g = Game()
+g.run()
